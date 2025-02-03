d3ad842d850d04d4b7d0bb4d21d21421f3be1c53
BrandonRada <brandonn.rada@gmail.com>
2025-02-02 23:54:19 -0800

    changed massage

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,7 +1,5 @@
 //Unused for lab 1.
 
 module.exports = {
-    messages: {
-        greetingMessage: "Hello %1, What a beautiful day."
-    }
+    greetingMessage: "Hello %1, What a beautiful day."
 };
